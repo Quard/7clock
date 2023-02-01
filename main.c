137e1fad283016b17f534bcd80e272af045e4a7b
Vadym Zakovinko <vadym@zakovinko>
2023-02-01 21:06:36 +0200

    control brightness using ldr sensor

--- main.c
+++ main.c
@@ -28,6 +28,9 @@
 
 void timer_init(void);
 void rtc_init(void);
+void adc_init(void);
+void pwm_init(void);
+void brightness_control(void);
 
 
 void main(void) {
@@ -38,6 +41,8 @@ void main(void) {
 
     display_init();
     rtc_init();
+    adc_init();
+    pwm_init();
     timer_init();
 
 //    ds1302_set_time(20, 41);
@@ -52,6 +57,7 @@ void main(void) {
 
         if (TMR1IF == 1) {
             ds1302_get_time(&hour, &minutes);
+            brightness_control();
             TMR1IF = 0;
         }
     }
@@ -66,5 +72,38 @@ void timer_init(void) {
 void rtc_init(void) {
     if (!ds1302_is_enabled()) {
         ds1302_enable();
+    }
+}
+
+void adc_init(void) {
+    ANSA1 = 1;
+    TRISA1 = 1;
+    ANSELA = 0x0 | (1 << 1);  // RA1(AN1) - analog
+    ADCON1 = 0b11000000; // Right justified, FOSC/4, Vref=Vcc
+    ADCON0 = 0b00000111; // AN1 activate, ADC on
+}
+
+void pwm_init(void) {
+    PR2 = 9;  //(_XTAL_FREQ / (100kHz * 4 * 1)) - 1;
+    T2CON = 0b00000100;  // TIM2 on, Prescaler 1
+    CCP1CON = 0b00001100;
+    TRISB3 = 0;
+    DC1B0 = 0;
+    DC1B1 = 0;
+}
+
+inline void pwm_set_duty(uint8_t duty) {
+    CCPR1L = duty;
+}
+
+void brightness_control(void) {
+    if ((ADCON0 & 0b10) == 0) {
+        uint16_t val = (uint16_t)(((ADRESH & 0b11) << 8) | ADRESL);
+        if (val < 400) {
+            pwm_set_duty(3);
+        } else {
+            pwm_set_duty(9);
+        }
+        ADCON0 |= (1 << 1);
     }
 }
